25d7f43f193f622ab9aebe7f9c7c0771de53a7a6
Stillpatience <felix.cammaerts@hotmail.com>
2019-09-18 19:57:16 +0200

    Solved issue 1

--- XMLBuilder.py
+++ XMLBuilder.py
@@ -8,25 +8,18 @@ from Line import Line
 from Point import Point
 
 
-def convert_lines_to_xml(lines):
-    result = []
-    for line in lines:
-        result.append(str(line))
-    return result
-
-
 def calculate_thickness(distance):
     return int(math.sqrt(2 * (distance ** 2)))
 
 
-def generate_xml(d=1, file_name="test.jpg", out_file="result.xml"):
-    im = Image.open(file_name)
+def get_maximum_size():
+    return 40000
+
+
+def generate_xml(d=1, img_directory="images", img_name="test.jpg", out_file="result.xml",  out_dir="outputs"):
+    im = Image.open(os.path.join(img_directory, img_name))
     pix = im.load()
-    first_y = 0
-    prev_color = pix[0, 0]
-    last_y = 0
-    curr_x = 0
-    lines = []
+    first_y, last_y, curr_x, lines, prev_color = 0, 0, 0, [], pix[0, 0]
     thickness = calculate_thickness(d)
     for x in range(0, im.size[0], d):
         for y in range(0, im.size[1], d):
@@ -35,23 +28,36 @@ def generate_xml(d=1, file_name="test.jpg", out_file="result.xml"):
                 last_y = y
             else:
                 if first_y == last_y:
-                    lines.append(Line(Point(x, first_y), Point(x, last_y+1), Color(prev_color), thickness))
+                    lines.append(
+                        Line(
+                            Point(x, first_y),
+                            Point(x, last_y + 1),
+                            Color(prev_color),
+                            thickness,
+                        )
+                    )
                 else:
-                    lines.append(Line(Point(x, first_y), Point(x, last_y), Color(prev_color), thickness))
+                    lines.append(
+                        Line(
+                            Point(x, first_y),
+                            Point(x, last_y),
+                            Color(prev_color),
+                            thickness,
+                        )
+                    )
                 prev_color = color
                 first_y = y
                 last_y = y
                 curr_x = x
     drawing = Drawing(lines)
 
-    if len(str(drawing)) < 60000:
+    if len(str(drawing)) < get_maximum_size():
         print("Reached an accuracy of: " + str(d))
-        output_file = open(out_file, "w")
+        output_file = open(os.path.join(out_dir, out_file), "w")
         output_file.write(str(drawing))
         return
     else:
-        generate_xml(d=d + 1, file_name=file_name, out_file=out_file)
+        generate_xml(d=d + 1, img_directory=img_directory, img_name=img_name, out_dir=out_dir, out_file=out_file)
 
 
-
-generate_xml(file_name="felix.jpg")
+generate_xml()
